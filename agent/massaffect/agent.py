4b3a48d9588bc23c07da76ea46ae912b3edf5ae5
Jeremy Moles <cubicool@gmail.com>
2026-02-28 16:59:28 -0500

    Replaces the '\0' byte in abstract socket name logging

--- agent/massaffect/agent.py
+++ agent/massaffect/agent.py
@@ -109,7 +109,7 @@ class Agent(Loggable):
 			path=config().SOCKET_NAME,
 		)
 
-		self.log.debug(f"Created socket: {config().SOCKET_NAME}")
+		self.log.debug(f"Created socket: {config().SOCKET_NAME.replace('\0', '@')}")
 
 		dispatcher_task = asyncio.create_task(self.dispatcher.run())
 		server_task = asyncio.create_task(self.server.serve_forever())
